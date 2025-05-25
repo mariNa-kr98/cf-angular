1100719edfd40d04aa72e1de8811b0b1cd405c33
Mar. Krommyda <kmarikro@gmail.com>
2025-05-25 17:26:12 +0300

    New component: person-table-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  name = 'Marina';
+
+  person = {
+    givenName: 'Marina',
+    surname: 'Krommyda',
+    age: 26,
+    email: 'kmarikro@aueb.gr'
+  }
 }
