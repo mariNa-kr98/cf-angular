f18bc94af28feae3b5454824adc2b39063ff2e78
Mar. Krommyda <kmarikro@gmail.com>
2025-06-02 17:43:24 +0300

    Output-example created. Asc-Desc simple-datatable.

---
+++ src/app/components/component-output-example/component-output-example.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-output-example',
+  imports: [],
+  templateUrl: './component-output-example.component.html',
+  styleUrl: './component-output-example.component.css'
+})
+export class ComponentOutputExampleComponent {
+
+}
