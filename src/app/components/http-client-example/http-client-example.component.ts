4000af93d62cf6010448b972701e33140f5398a4
Mar. Krommyda <kmarikro@gmail.com>
2025-06-06 18:48:05 +0300

    Client example and services created

---
+++ src/app/components/http-client-example/http-client-example.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-http-client-example',
+  imports: [],
+  templateUrl: './http-client-example.component.html',
+  styleUrl: './http-client-example.component.css'
+})
+export class HttpClientExampleComponent {
+
+}
