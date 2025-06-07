c044a3b4c4da4efc1613edee52dd16e7b1fddb8f
Mar. Krommyda <kmarikro@gmail.com>
2025-06-07 21:42:07 +0300

    Http example created

--- src/app/components/http-client-example/http-client-example.component.ts
+++ src/app/components/http-client-example/http-client-example.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { JokesService } from 'src/app/shared/services/jokes.service';
+import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { subscribeOn } from 'rxjs';
 
 @Component({
   selector: 'app-http-client-example',
-  imports: [],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './http-client-example.component.html',
   styleUrl: './http-client-example.component.css'
 })
-export class HttpClientExampleComponent {
+export class HttpClientExampleComponent implements OnInit{
+  jokesService = inject(JokesService);
 
+  dadJoke: string = '';
+  chuckNorrisJoke: string = '';
+
+  ngOnInit(): void{
+    // this.jokesService.getDadJokes()
+    // .subscribe((data)=>{
+    //   console.log("DAD JOKE", data)
+    //   console.log("Dad joke", data.joke)
+    // })
+
+    // this.jokesService.getChuckNorrisJoke()
+    // .subscribe((data)=>{
+    //   console.log("CHUCK NORRIS", data)
+    //   console.log("Chuck joke", data.value)
+    // })
+    this.refreshChuckNorrisJoke();
+    this.refreshDadJoke();
+  }
+
+  refreshDadJoke(){
+    this.jokesService.getDadJokes()
+      .subscribe((data)=>{
+       console.log("Dad Joke", data.joke)
+       this.dadJoke = data.joke;
+    })
+  }
+
+  refreshChuckNorrisJoke(){
+    this.jokesService.getChuckNorrisJoke()
+      .subscribe((data)=>{
+        console.log("Chuck Norris Joke", data.value)
+        this.chuckNorrisJoke = data.value;
+      })
+  }
 }
--- src/app/shared/services/jokes.service.ts
+++ src/app/shared/services/jokes.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DadJoke, ChuckNorrisJoke } from '../interfaces/jokes';
+
+const DAD_JOKES_API_URL = 'https://icanhazdadjoke.com/';
+const JACK_NORRIS_JOKES_API = 'https://api.chucknorris.io/jokes/random';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JokesService {
+  http: HttpClient = inject(HttpClient);
 
   constructor() { }
+
+  getDadJokes(){
+    return this.http.get<DadJoke>(DAD_JOKES_API_URL, {
+      headers: {
+        Accept: "application/json"
+      }
+    })
+  }
+
+  getChuckNorrisJoke(){
+    return this.http.get<ChuckNorrisJoke>(JACK_NORRIS_JOKES_API, {
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+  }
 }
