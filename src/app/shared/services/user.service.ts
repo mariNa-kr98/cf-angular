beb082a1475f292b4bfbe2611deaa21f4006bd38
Mar. Krommyda <kmarikro@gmail.com>
2025-06-11 19:38:28 +0300

    User login comp. created

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -2,9 +2,10 @@ import { inject, Injectable } from '@angular/core';
 import { Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
-import { User } from '../interfaces/user';
+import { User, Credentials } from '../interfaces/user';
 
 const API_URL = `${environment.apiURL}/api/users`
+const API_URL_AUTH = `${environment.apiURL}/api/auth`
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,12 @@ export class UserService {
   check_dublicate_email(email: string){
     return this.http.get<{status:boolean, data:User}>(
       `${API_URL}/check_duplicate_email/${email}`
+    )
+  }
+
+  loginUser(credentials: Credentials){
+    return this.http.post<{status:boolean, data:string}>(
+      `${API_URL_AUTH}/login`, credentials
     )
   }
 }
